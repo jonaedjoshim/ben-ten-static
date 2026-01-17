4cc720c799bf731a519d8e1e814a9e4905be69d8
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-17 18:32:30 +0600

    deploying to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Tomar repo-r nam 'ben-ten-static' tai ekhane sheta add kora holo
-  base: "/ben-ten-static/",
+  // base: "/ben-ten-static/",
   plugins: [react(), tailwindcss()],
 });
